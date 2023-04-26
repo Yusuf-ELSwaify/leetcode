bdb1f6870d234c79bf8dedea45ede10019796179
Yusuf EL-Swaify <78337527+Yusuf-ELSwaify@users.noreply.github.com>
2023-04-26 06:57:19 +0200

    Time: 1 ms (100.00%), Space: 39.6 MB (85.75%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,14 +1,10 @@
 class Solution {
     public int addDigits(int num) {
-        int sum = 0;
-        while (num > 0){
-            sum += num % 10;
+        while (num > 9) {
+            int remainder = num % 10;
             num /= 10;
-            if (num == 0 && sum > 9){
-                num = sum;
-                sum = 0;
-            }
+            num += remainder;
         }
-        return sum;
+        return num;
     }
 }
