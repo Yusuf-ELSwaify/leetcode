3de3f1612f7939b8a2b4f06ef57283138d27d02a
Yusuf EL-Swaify <78337527+Yusuf-ELSwaify@users.noreply.github.com>
2023-06-09 05:43:36 +0300

    Time: 0 ms (100.00%), Space: 44.2 MB (27.33%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,26 +1,21 @@
 class Solution {
-    int binarySearch(char[] letters, char target, int start, int end) {
+    char target;
+    int binarySearch(char[] letters, int start, int end) {
         int mid = (start + end) / 2;
 
-        if (letters[mid] == target)
-            return mid + 1;
-        if (start >= end)
-            return mid;
-        if (letters[mid] > target)
-            return binarySearch(letters, target, start, mid - 1); 
+        if (start > end)
+            return start;
         
-        return binarySearch(letters, target, mid + 1 , end);//if (letters[mid] < 0)
+        if (letters[mid] > target)
+            return binarySearch(letters, start, mid - 1); 
+        
+        return binarySearch(letters, mid + 1 , end);//if (letters[mid] <= target)
     }
     public char nextGreatestLetter(char[] letters, char target) {
-        if(letters[0] > target)
-          return letters[0];
-        else if(target >= letters[letters.length - 1])
-            return letters[0];  
-
-        int firstOccurrence = binarySearch(letters, target, 0, letters.length - 1);
-        while (firstOccurrence < letters.length - 1 && letters[firstOccurrence] <= target)
-            ++firstOccurrence;
-
-        return letters[firstOccurrence];
+        this.target = target;
+        int resultIndex = binarySearch(letters, 0, letters.length - 1);
+        resultIndex = resultIndex == letters.length ? 0 : resultIndex;
+ 
+        return letters[resultIndex];
     }
 }
